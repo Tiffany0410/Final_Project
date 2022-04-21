e040aafce0e79b6afaf9da3837a5e757802e23f6
shitang <tiffanyshi.tang@mail.utoronto.ca>
2022-04-21 11:17:14 -0700

    update

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 This is my JSC370 Final Project website home. The website is online at https://tiffany0410.github.io/Final_Project/
+
+## General information about the project, title, brief description, etc
--- process_data.R
+++ process_data.R
@@ -1,17 +1,25 @@
-data <- read_csv("life_expectancy_data.csv")
-
+data <- read_csv("data/life_expectancy_data.csv")
 data <- data %>%
-  rename("Life_expectancy" = "Life expectancy", "Hepatitis_B" = "Hepatitis B", "percentage_expenditure" = "percentage expenditure",
-         "total_expenditure" = "Total expenditure")
+  rename("Life_expectancy" = "Life expectancy", "Adult_Mortality" = "Adult Mortality", "infant_deaths" = "infant deaths", "percentage_expenditure" = "percentage expenditure", "Hepatitis_B" = "Hepatitis B", "under_five_deaths" = "under-five deaths", "Total_expenditure" = "Total expenditure", "thinness_1_19_years" = "thinness  1-19 years", "thinness_5_9_years" = "thinness 5-9 years", "Income_composition_of_resources" = "Income composition of resources", "HIV_or_AID" = "HIV/AIDS")
 
-filter_data <- data %>%
-  select(c("Country", "Year", "Life_expectancy", "Hepatitis_B", "Polio", "Diphtheria", "GDP", "Population", "percentage_expenditure", "total_expenditure")) %>%
-  filter(Year == 2014)
-
-NA_columns <- sapply(filter_data, function(x) sum(is.na(x)))
+NA_columns <- sapply(data, function(x) sum(is.na(x)))
+names(NA_columns[NA_columns > 0])
 
 for (i in names(NA_columns[NA_columns > 0])) {
-  filter_data[, i][is.na(filter_data[, i])] <- as.numeric(lapply(filter_data[, i], mean, na.rm = TRUE))
+  data[, i][is.na(data[, i])] <- as.numeric(lapply(data[, i], mean, na.rm = TRUE))
 }
 
-long_data <- gather(filter_data, vaccine, coverage, Hepatitis_B:Diphtheria, factor_key=TRUE)
+data <- data %>%
+  mutate(developed = ifelse(Status == 'Developed', 1, 0))
+
+data <- data %>%
+  select(-Status)
+
+normalize <- function(x){
+  round((x-min(x))/(max(x)-min(x)), 4)
+}
+
+data[3:21] <- as.data.frame(lapply(data[3:21], normalize))
+
+long_data <- gather(data, parameter, value, Adult_Mortality:developed, factor_key=TRUE)
+long_data_2 <- gather(data, parameter, value, Life_expectancy:developed, factor_key=TRUE)
